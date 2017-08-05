3dae15d5f021e201c3b180f915c46f566dc3fec1
root <webdev01124@gmail.com>
2017-08-05 13:40:02 -0700

    PropertyTypeLang.php updated

--- app/Models/admin/PropertyTypeLang.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models\admin;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class PropertyTypeLang extends Authenticatable
-{
-    protected $table = 'property_type_lang';
-}
